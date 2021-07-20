3175adb8a1aacadaffbfd93cd2fce3270b0a5d1e
mahendra214 <56427598+mahendra214@users.noreply.github.com>
2021-07-20 14:20:33 +0530

    Add files via upload
    first commit

---
+++ demo1/src/main/java/demo1/Test1.java
@@ -0,0 +1,23 @@
+package demo1;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("demo1");
+		EntityManager manager = factory.createEntityManager();
+
+		Employee emp1 = new Employee(101, "Yashwant", "Mumbai", 1000.00);
+		EntityTransaction tx = manager.getTransaction();
+
+		tx.begin();
+		manager.persist(emp1);
+		tx.commit();
+
+	}
+
+}
